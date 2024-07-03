51fd7a3e9e154f8be2e39472497c0abbc479693f
Kris Haroun <kris.haroun@outlook.com>
2024-07-03 13:26:37 +0100

    fixed pagination, created registration and login

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import bcrypt from "bcrypt";
+import session from "express-session";
 
 const app = express();
 const port = 3000;
@@ -18,8 +20,59 @@ const db = new pg.Client({
 
 db.connect();
 
+app.use(session({
+    secret: 'your_secret_key',
+    resave: false,
+    saveUninitialized: true
+}));
+
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    const user = req.session.user || null;
+    res.render("index.ejs", { user });
+});
+
+app.get("/gym_design", (req, res) => {
+    res.render("gym_design.ejs");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+});
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    const user = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+
+    if (user.rows.length > 0) {
+        const match = await bcrypt.compare(password, user.rows[0].password);
+        if (match) {
+            req.session.user = user.rows[0];
+            res.redirect("/");
+        } else {
+            res.send("Incorrect password");
+        }
+    } else {
+        res.send("No user found with that username");
+    }
+});
+
+app.get("/register", (req, res) => {
+    res.render("register.ejs");
+})
+
+app.post("/register", async (req, res) => {
+    const { first_name, last_name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+        await db.query(
+            "INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)",
+            [first_name, last_name, email, hashedPassword]
+        );
+        res.redirect("/login");
+    } catch (e) {
+        res.send("Error registering user");
+    }
 });
 
 app.get("/:category", async (req, res) => {
